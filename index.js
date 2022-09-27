eccd1f96581f6f6b7e7603d89feff9a887fc68fd
Guilherme Galaverna <guigalaverna@gmail.com>
2022-09-27 22:58:58 +0000

    feat: finished app

---
+++ index.js
@@ -0,0 +1,23 @@
+async function getResumesList() {
+    const resumeList = await axios.get('/resume-list.json')
+
+    return resumeList.data
+}
+
+async function populatePage() {
+    const listElement = document.querySelector('ul')
+
+    const resumeList = await getResumesList()
+
+    resumeList.map(resumeItem => {
+        const listItemElement = document.createElement('li')
+
+        listItemElement.innerHTML = `<a href="${resumeItem.source}">${resumeItem.subject} - ${resumeItem.name}</a>`
+
+        listElement.appendChild(listItemElement)
+    })
+
+    
+}
+
+populatePage()
